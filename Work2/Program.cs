5e06fccf49c5ba91bc5ff0f149d16eecb6c2e4fc
AlevtinaIudanova <iudanova87@bk.ru>
2023-03-16 14:36:57 +0100

    Найти сумму элементов от M до N, N и M заданы

---
+++ Work2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int Summ(int M, int N)
+{
+    if (M == N)
+    {
+        return M;
+    }
+    else return N + Summ(M, N - 1);
+}
+Console.WriteLine("Enter M: ");
+int SummM = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Enter N: ");
+int SummN = int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine($"{Summ(SummM, SummN)}");
