24fa21fcd3db4c341982ad7edf7b919d99fe0a8a
AlevtinaIudanova <iudanova87@bk.ru>
2023-03-16 14:38:57 +0100

    Написать программу возведения числа А в целую стень B

---
+++ Work5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+Console.Write("Enter A number: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.Write("Enter B step: ");
+int stepB = int.Parse(Console.ReadLine());
+
+int result(int A, int B)
+{
+    if (B == 0) return 1;
+    else return result(A, B - 1) * A;
+}
+
+
+Console.WriteLine($"{result(numberA, stepB)}");
