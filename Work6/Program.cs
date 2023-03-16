47b9e202dd039f9888a56036d2ca3c446de044c0
AlevtinaIudanova <iudanova87@bk.ru>
2023-03-16 14:39:40 +0100

    Написать программу показывающие первые N чисел, для которых каждое с //ледующее равно сумме двух предыдущих. Первые два элемента //последовательности задаются пользователем

---
+++ Work6/Program.cs
@@ -0,0 +1,24 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое с
+//ледующее равно сумме двух предыдущих. Первые два элемента 
+//последовательности задаются пользователем
+
+
+
+
+Console.Write("Enter FirstNumber: ");
+int n1 = int.Parse(Console.ReadLine()!);
+
+
+Console.Write("Enter SecondNumber: ");
+int n2 = int.Parse(Console.ReadLine()!);
+
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
+for (int i = n1; i < 10; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
