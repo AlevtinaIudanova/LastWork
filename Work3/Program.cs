2ae8674585ae1511cf217433db030ab76f829d77
AlevtinaIudanova <iudanova87@bk.ru>
2023-03-16 14:38:07 +0100

    Найти сумму цифр числа

---
+++ Work3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму цифр числа
+
+
+int SumNumber(int number)
+{
+    if (number == 0)
+        return 0;
+    return SumNumber(number / 10) + number % 10;
+}
+Console.Write("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(SumNumber(number));
