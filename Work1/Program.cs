35a21912633d3f88ff0d4de16f6a863906fa4f3e
AlevtinaIudanova <iudanova87@bk.ru>
2023-03-16 14:35:29 +0100

    Показать натуральные числа от M до N, N и M заданы

---
+++ Work1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+/*int M;
+Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine()!, out M);
+
+int N;
+Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine()!, out N);
+
+int index = 1;
+while (index <= N)
+{
+
+
+    Console.Write($"{index} ");
+
+    index++;
+}*/
+
+string NumbersRec(int M, int N)
+{
+    if (M <= N) return M + NumbersRec(M + 1, N);
+    else return string.Empty;
+}
+
+Console.WriteLine("Enter M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter N: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(NumbersRec(M, N));
